689e524901025cb926fce25a36a2a5cfb61e5b39
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-26 09:18:46 -0500

    add camelcase alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	for (c = 'A'; c <= 'Z'; ++c)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
