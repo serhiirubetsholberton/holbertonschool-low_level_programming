f11df0626739d388eeb242383bc1201cf561b771
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-26 16:10:34 -0500

    change atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,13 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
- *
- * Return: The integer value of the converted string.
- */
+// A simple atoi() function
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
+	// Initialize result
+	int res = 0;
 
-	while (*s++)
-	{
-		if (*s == '-')
-			sign *= -1;
+	for (int i = 0; s[i] != '\0'; ++i)
+		res = res * 10 + s[i] - '0';
 
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-	};
-
-	return (num * sign);
+	return res;
 }
