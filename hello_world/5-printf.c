0fd202b2bfd1c5e922a36dc5b2b46cb4bb83c1df
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-26 08:21:56 -0500

    change tab

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    printf("\"Programming is like building a multilingual puzzle");
-    return (0);
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
