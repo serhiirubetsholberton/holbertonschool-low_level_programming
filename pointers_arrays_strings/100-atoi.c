3d6d7a825fee803804accd68bfb400876195b119
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-26 16:16:58 -0500

    added minus support

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,15 +8,20 @@
  */
 int _atoi(char *s)
 {
-	int i;
+
 	int res = 0;
 	int sign = 1;
+	int i = 0;
 
-	if (*s == '-')
-		sign *= -1;
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
 
-	for (i = 0; s[i] != '\0'; ++i)
+	for (; s[i] != '\0'; ++i)
 		res = res * 10 + s[i] - '0';
 
-	return (res * sign);
+	// Return result with sign
+	return sign * res;
 }
