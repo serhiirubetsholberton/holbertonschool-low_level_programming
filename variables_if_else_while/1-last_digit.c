26a55edba15aa1468d0476d6fdff035d24a8dafb
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-26 09:03:48 -0500

    fix betty errors

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -13,11 +16,19 @@ int main(void)
 	int lastNumber = n % 10;
 
 	if (lastNumber > 5)
+	{
 		printf("Last digit of %i is %i and is greater than 5\n", n, lastNumber);
-	else if (lastNumber == 0)
+	}
+	if (lastNumber == 0)
+	{
 		printf("Last digit of %i is %i and is 0\n", n, lastNumber);
-	else if (lastNumber < 6)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastNumber);
+	}
+	if (lastNumber < 6)
+	{
+		char text[] = "Last digit of %i is %i and is less than 6 and not 0\n";
+
+		printf(text, n, lastNumber);
+	}
 
 	return (0);
 }
