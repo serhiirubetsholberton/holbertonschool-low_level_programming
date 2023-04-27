73c5e158713d05f1f832ce057f14ae8d5f174f07
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-27 11:23:32 -0500

    fixes

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,50 +1,13 @@
 #include "main.h"
 
 /**
- * _strcmp - Compares two strings
- * @s1: The first string
- * @s2: The second string
- *
- * Return: int value
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
  */
-int _strcmp(char *s1, char *s2)
+void _puts(char *str)
 {
-	int firstStrLength = 0, secondStrLength = 0, i = 0, r = 0, limit;
+	while (*str)
+		_putchar(*str++);
 
-	while (s1[firstStrLength])
-	{
-		firstStrLength++;
-	}
-
-	while (s2[secondStrLength])
-	{
-		secondStrLength++;
-	}
-
-	if (firstStrLength <= secondStrLength)
-	{
-		limit = firstStrLength;
-	}
-	else
-	{
-		limit = secondStrLength;
-	}
-
-	while (i <= limit)
-	{
-		if (s1[i] == s2[i])
-		{
-			i++;
-			continue;
-		}
-		else
-		{
-			r = s1[i] - s2[i];
-			break;
-		}
-
-		i++;
-	}
-
-	return (r);
+	_putchar('\n');
 }
